0cdc1b0934a05073add830a2172a9772383bbd59
Eddo <eddomora@gmail.com>
2025-06-10 13:56:39 -0400

    Ingreso de archivos base para iniciar el proyecto

---
+++ Proyecto_Final/src/main/java/org/example/Visual/Aplicacion.java
@@ -0,0 +1,9 @@
+package org.example.Visual;
+
+public class Aplicacion {
+    private static Ventana ventana;
+    public Aplicacion(){
+        ventana = new Ventana();
+
+    }
+}
---
+++ Proyecto_Final/src/main/java/org/example/Visual/Ventana.java
@@ -0,0 +1,14 @@
+package org.example.Visual;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana extends JFrame {
+
+    public Ventana(){
+        setTitle("Organizador de Competencias");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(1214, 716));
+    }
+
+}
