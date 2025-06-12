a69e80c71370d7942095a1c958b396bae70b561e
Eddo <eddomora@gmail.com>
2025-06-12 14:22:29 -0400

    Creacion de metodos de arraylist para la clase de Equipos

--- Proyecto_Final/src/main/java/org/example/Logica/Equipos.java
+++ Proyecto_Final/src/main/java/org/example/Logica/Equipos.java
@@ -1,13 +1,33 @@
 package org.example.Logica;
+import java.util.ArrayList;
 
-public class Equipos {
+
+public class Equipos<T> {
     private String nombre;
+    private ArrayList<T> equipo;
 
     public Equipos(String nombre){
         this.nombre = nombre;
+        equipo = new ArrayList<T>();
+
     }
 
-    public String getNombre() {
+    public String getNombreEquipo() {
         return nombre;
     }
+
+    /**
+     * Agrega un persona al equipo.
+     *
+     * @param persona es la persona que se desea agregar.
+     */
+    public void addPersona(T persona){
+        equipo.add(persona);
+    }
+    
+    public int size(){
+        return equipo.size();
+    }
+
 }
+
