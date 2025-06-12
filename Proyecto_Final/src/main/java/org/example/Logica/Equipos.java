6bd868036d88223d22432408facf8c4a8015b0f5
Eddo <eddomora@gmail.com>
2025-06-12 14:14:33 -0400

    Creacion de clases para una persona y para equipos

---
+++ Proyecto_Final/src/main/java/org/example/Logica/Equipos.java
@@ -0,0 +1,13 @@
+package org.example.Logica;
+
+public class Equipos {
+    private String nombre;
+
+    public Equipos(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
