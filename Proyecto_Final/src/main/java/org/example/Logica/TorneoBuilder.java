7c7ead6c1edf06022f46bc859fbc766075c8554a
Vicente <vimiranda2023@udec.cl>
2025-06-28 17:23:51 -0400

    Agrega clase builder para crear Torneos sin sobrecargar el constructor

---
+++ Proyecto_Final/src/main/java/org/example/Logica/TorneoBuilder.java
@@ -0,0 +1,43 @@
+package org.example.Logica;
+
+import java.time.LocalDate;
+
+public class TorneoBuilder {
+
+    private String nombreTorneo;
+    private FORMATO formatoTorneo;
+    private TIPOPARTICIPANTES tipoParticipantes;
+    private String lugarTorneo;
+    private LocalDate comienzoTorneo;
+    private String premiosTorneo;
+
+    public TorneoBuilder conNombre(String nombre) {
+        this.nombreTorneo = nombre;
+        return this;
+    }
+
+    public TorneoBuilder conFormato(FORMATO formato) {
+        this.formatoTorneo = formato;
+        return this;
+    }
+
+    public TorneoBuilder conTipoParticipantes(TIPOPARTICIPANTES tipoparticipante) {
+        this.tipoParticipantes = tipoparticipante;
+        return this;
+    }
+
+    public TorneoBuilder conLugar(String lugar) {
+        this.lugarTorneo = lugar;
+        return this;
+    }
+
+    public TorneoBuilder conFechaInicio(LocalDate fecha) {
+        this.comienzoTorneo = fecha;
+        return this;
+    }
+
+    public TorneoBuilder conPremios(String premios) {
+        this.premiosTorneo = premios;
+        return this;
+    }
+}
